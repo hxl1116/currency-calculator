a05cb99e1772b15be5f6a1db968b0c741f1c1149
Henry Larson <henryclarson22@gmail.com>
2023-04-11 20:20:58 -0400

    Added `Conversion` struct, conversion func to USD; Added TODOs

--- src/conv.rs
+++ src/conv.rs
@@ -1,4 +1,24 @@
-// TODO: Implement conversion function
-pub fn get_conv(from_rate:String, to_rate:String, amt: f64) -> () {
-    todo!("Implement conversion func")
+use serde::Deserialize;
+
+use crate::rate::{get_forex_rate, ForexRate};
+
+#[derive(Debug, Default)]
+pub struct Conversion {
+    pub symbol: String,
+    pub value: f64,
 }
+
+pub fn get_conv_to_usd(amt: f64, from: String) -> Conversion {
+    let from_rate: ForexRate = get_forex_rate(from).unwrap();
+
+    Conversion {
+        symbol: from_rate.symbol,
+        value: amt * from_rate.inverse,
+    }
+}
+
+// TODO: Implement conversion func from USD to target currency
+pub fn get_conv_from_usd() -> () {}
+
+// TODO: Implement conversion func for currency parameters
+pub fn get_conv(amt: f64, from: String, to: String) -> () {}
