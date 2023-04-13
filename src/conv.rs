53b72766ed3f15f5c61f1d0360782cf985a7d553
Henry Larson <henryclarson22@gmail.com>
2023-04-12 20:15:19 -0400

    Renamed `ForexRate` struct to `ForexConversionInfo`, `symbol` field to `code`

--- src/conv.rs
+++ src/conv.rs
@@ -1,8 +1,8 @@
-use crate::rate::{get_forex_rate, ForexRate};
+use crate::rate::{get_forex_rate, ForexConversionInfo};
 
 pub fn convert_currency(amt: f64, from: String, to: String) -> f64 {
-    let from_rate: ForexRate = get_forex_rate(from).unwrap();
-    let to_rate: ForexRate = get_forex_rate(to).unwrap();
+    let from_rate: ForexConversionInfo = get_forex_rate(from).unwrap();
+    let to_rate: ForexConversionInfo = get_forex_rate(to).unwrap();
     
     amt * from_rate.inverse * to_rate.rate
 }
--- src/rate.rs
+++ src/rate.rs
@@ -3,8 +3,8 @@ use serde::Deserialize;
 
 #[derive(Debug, Default, Deserialize)]
 #[serde(rename_all = "PascalCase")]
-pub struct ForexRate {
-    pub symbol: String,
+pub struct ForexConversionInfo {
+    pub code: String,
     pub name: String,
     pub rate: f64,
     pub inverse: f64,
@@ -21,12 +21,12 @@ pub fn get_forex_symbols() -> RedisResult<Vec<String>> {
     Ok(result)
 }
 
-pub fn get_forex_rate(forex_symbol: String) -> RedisResult<ForexRate> {
+pub fn get_forex_rate(forex_symbol: String) -> RedisResult<ForexConversionInfo> {
     let mut conn = redis_client().get_connection().unwrap();
     let result: Vec<String> = conn.hvals(format!("forex_rate:{}", forex_symbol)).unwrap();
 
-    let rate = ForexRate {
-        symbol: result[0].clone(),
+    let rate = ForexConversionInfo {
+        code: result[0].clone(),
         name: result[1].clone(),
         rate: result[2].parse().unwrap(),
         inverse: result[3].parse().unwrap(),
