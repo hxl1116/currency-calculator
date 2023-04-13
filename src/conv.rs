ab810ec2fab069b2b4490a3859290ae82c5671c9
Henry Larson <henryclarson22@gmail.com>
2023-04-12 20:01:50 -0400

    Removed redundant conversion func

--- src/conv.rs
+++ src/conv.rs
@@ -1,5 +1,3 @@
-use serde::Deserialize;
-
 use crate::rate::{get_forex_rate, ForexRate};
 
 #[derive(Debug, Default)]
@@ -8,30 +6,12 @@ pub struct Conversion {
     pub value: f64,
 }
 
-pub fn get_conv_to_usd(amt: f64, from: String) -> Conversion {
-    let from_rate: ForexRate = get_forex_rate(from).unwrap();
-
-    Conversion {
-        symbol: from_rate.symbol,
-        value: amt * from_rate.inverse
-    }
-}
-
-pub fn get_conv_from_usd(amt: f64, to: String) -> Conversion {
-    let to_rate: ForexRate = get_forex_rate(to).unwrap();
-
-    Conversion {
-        symbol: to_rate.symbol,
-        value: amt * to_rate.rate
-    }
-}
-
-pub fn get_conv(amt: f64, from: String, to: String) -> Conversion {
+pub fn convert_currency(amt: f64, from: String, to: String) -> Conversion {
     let from_rate: ForexRate = get_forex_rate(from).unwrap();
     let to_rate: ForexRate = get_forex_rate(to).unwrap();
 
     Conversion {
         symbol: to_rate.symbol,
-        value: amt * from_rate.inverse * to_rate.rate
+        value: amt * from_rate.inverse * to_rate.rate,
     }
 }
