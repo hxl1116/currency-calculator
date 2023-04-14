b17b466fb4820af878ad8238dbe718c924e30e8e
Henry Larson <henryclarson22@gmail.com>
2023-04-13 22:56:17 -0400

    lua functions stubbed out, TODOs added

---
+++ scripts/forex.lua
@@ -0,0 +1,18 @@
+local CURRENCY_CODE_NAMESPACE = 'currency_codes'
+local CURRENCY_RATES_NAMESPACE = 'currency_rates'
+local CONVERSION_INFO_NAMESPACE = 'conversion_info'
+
+-- TODO: Implement fetching currency codes from Redis
+local function get_currency_codes()
+
+end
+
+-- TODO: Implement fetching currency rates from Redis
+local function get_currency_rates()
+
+end
+
+-- TODO: Implement fetching conversion info from Redis
+local function get_conversion_info(currency_code)
+
+end
