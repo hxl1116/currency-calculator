4f1f995b9f55cb3171deb17f77b82281d093e0f1
Henry Larson <henryclarson22@gmail.com>
2023-04-20 18:14:43 -0400

    Implemented lua func for getting currency codes, rates, and info

--- scripts/forex.lua
+++ scripts/forex.lua
@@ -1,18 +1,24 @@
+#!lua name=forexlib
+
+local REDIS_HOSTNAME = 'localhost'
+local REDIS_PORT = '6379'
+
 local CURRENCY_CODE_NAMESPACE = 'currency_codes'
 local CURRENCY_RATES_NAMESPACE = 'currency_rates'
 local CONVERSION_INFO_NAMESPACE = 'conversion_info'
 
--- TODO: Implement fetching currency codes from Redis
 local function get_currency_codes()
-
+    return redis.call('hgetall', CURRENCY_CODE_NAMESPACE)
 end
 
--- TODO: Implement fetching currency rates from Redis
 local function get_currency_rates()
-
+    return redis.call('hgetall', CURRENCY_RATES_NAMESPACE)
 end
 
--- TODO: Implement fetching conversion info from Redis
 local function get_conversion_info(currency_code)
-
+    return redis.call('hgetall', CONVERSION_INFO_NAMESPACE .. ':' .. currency_code)
 end
+
+redis.register_function('forex_codes', get_currency_codes)
+redis.register_function('forex_rates', get_currency_rates)
+redis.register_function('conversion_info', get_conversion_info)
